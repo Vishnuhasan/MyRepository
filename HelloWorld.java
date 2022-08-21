8f2952db4dd570a9cbe65d26ea2f2a54bbb52340
Vishnuhasan <vishnuhasan2016@gmail.com>
2022-08-21 19:18:30 +0530

    commit in terminal

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+class HelloWorld{  
+    public static void main(String args[]){  
+     System.out.println("Hello Vishnu");  
+     System.out.println("Hello Hasan");  
+      System.out.println("Hello Hasan");  
+       System.out.println("Hello Vishnuhasan");  
+    }  
+}  
